c2ca8783905ff073616551e00f75ff82df5d4972
Egg Syntax <egg@novonon.com>
2021-03-20 11:26:27 -0400

    lein new breakloop-repl

---
+++ test/breakloop_repl/core_test.clj
@@ -0,0 +1,7 @@
+(ns breakloop-repl.core-test
+  (:require [clojure.test :refer :all]
+            [breakloop-repl.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
