30c619d77df3766830112ace4ee04ae863e0642e
Egg Syntax <egg@novonon.com>
2022-02-23 22:31:00 -0500

    Commit that may clash

--- src/breakloop_repl/core.clj
+++ src/breakloop_repl/core.clj
@@ -1,6 +1,25 @@
 (ns breakloop-repl.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defmacro break []
+  `(clojure.main/repl
+    :prompt #(print "debug=> ")
+    :read readr
+    :eval (partial contextual-eval (local-context))))
+
+(defn blrep
+  "Start a REPL whose behavior on error is to open an internal REPL preloaded
+  with the context of the error."
+  []
+  )
+
+;; (defn f [x]
+;;   ;; (println "/" (type (var (symbol 'x))))
+;;   #dbg
+;;   (if (> x 4)
+;;     (println "big x" x)
+;;     (do
+;;       (if (even? x)
+;;        (println "small even x" x)
+;;        (println "small odd x" x)))))
+
+;; (f 3)
